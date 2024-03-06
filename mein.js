810d1a46ffad44f0d0263a57ec21846569466e3c
Irene Marco <162420792+jeffnazal@users.noreply.github.com>
2024-03-06 09:41:43 +0200

    Create mein.js

---
+++ mein.js
@@ -0,0 +1,26 @@
+var columns;
+
+		if(typeof subject == "object"){
+			
+			if(subject instanceof Column){
+				//subject is column element
+				return subject;
+			}else if(subject instanceof ColumnComponent){
+				//subject is public column component
+				return subject._getSelf() || false;
+			}else if(typeof HTMLElement !== "undefined" && subject instanceof HTMLElement){
+
+      var matches = [];
+		
+		Object.keys(this.columnsByField).forEach((field) => {
+			var fieldRoot = this.table.options.nestedFieldSeparator ? field.split(this.table.options.nestedFieldSeparator)[0] : field;
+			if(fieldRoot === root){
+				matches.push(this.columnsByField[field]);
+			}
+		});
+		var index = this.columnsByIndex.findIndex((col) => {
+			return col.visible;
+		});
+		
+		return index > -1 ? this.columnsByIndex[index] : false;
+	}
